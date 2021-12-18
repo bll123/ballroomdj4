2c0ab24765728b880ffec9ba28213090496be38e
Brad Lanam <brad.lanam.comp@gmail.com>
2021-12-18 14:10:20 -0800

    chg to in-place merge sort

--- src/list.c
+++ src/list.c
@@ -9,9 +9,9 @@
 
 static long listBinarySearch (const list_t *, void *, listCompare_t);
 static int  nameValueCompare (void *, void *, listCompare_t);
-static void listSwap (list_t *, long, long);
-static void heapify (list_t *, long, long, listCompare_t);
-static void heapSort (list_t *, listCompare_t);
+static int  listCompare (const list_t *, void *, void *, listCompare_t);
+static void merge (list_t *, size_t, size_t, size_t, listCompare_t);
+static void mergeSort (list_t *, size_t, size_t, listCompare_t);
 
 /* best for short lists of data   */
 /* data size must be consistent   */
@@ -144,7 +144,7 @@ listFind (list_t *list, void *data, listCompare_t compare)
 void
 listSort (list_t *list, listCompare_t compare)
 {
-  heapSort (list, compare);
+  mergeSort (list, 0, list->count - 1, compare);
   list->ordered = LIST_ORDERED;
 }
 
@@ -228,6 +228,19 @@ nameValueCompare (void *d1, void *d2, listCompare_t compare)
   return rc;
 }
 
+static int
+listCompare (const list_t *list, void *a, void *b, listCompare_t compare)
+{
+  int     rc;
+
+  if (list->type == LIST_NAMEVALUE) {
+    rc = nameValueCompare (a, b, compare);
+  } else {
+    rc = compare (a, b);
+  }
+  return rc;
+}
+
 /* returns the location after as a negative number if not found */
 static long
 listBinarySearch (const list_t *list, void *data, listCompare_t compare)
@@ -242,11 +255,7 @@ listBinarySearch (const list_t *list, void *data, listCompare_t compare)
   while (l <= r) {
     m = l + (r - l) / 2;
 
-    if (list->type == LIST_NAMEVALUE) {
-      rc = nameValueCompare (list->data[m], data, compare);
-    } else {
-      rc = compare (list->data[m], data);
-    }
+    rc = listCompare (list, list->data[m], data, compare);
     if (rc == 0) {
       return m;
     }
@@ -263,63 +272,49 @@ listBinarySearch (const list_t *list, void *data, listCompare_t compare)
   return rm;
 }
 
+/*
+ * in-place merge sort from:
+ * https://www.geeksforgeeks.org/in-place-merge-sort/
+ */
+
 static void
-listSwap (list_t *list, long a, long b) {
-  void *temp = list->data[a];
-  list->data[a] = list->data[b];
-  list->data[b] = temp;
+merge (list_t *list, size_t start, size_t mid, size_t end, listCompare_t compare)
+{
+  size_t start2 = mid + 1;
+
+  int rc = listCompare (list, list->data [mid], list->data [start2], compare);
+  if (rc <= 0) {
+    return;
+  }
+
+  while (start <= mid && start2 <= end) {
+    rc = listCompare (list, list->data [start], list->data [start2], compare);
+    if (rc <= 0) {
+      start++;
+    } else {
+      void * value = list->data [start2];
+      size_t index = start2;
+
+      while (index != start) {
+        list->data [index] = list->data [index - 1];
+        index--;
+      }
+      list->data [start] = value;
+
+      start++;
+      mid++;
+      start2++;
+    }
+  }
 }
 
 static void
-heapify (list_t *list, long n, long i, listCompare_t compare)
+mergeSort (list_t *list, size_t l, size_t r, listCompare_t compare)
 {
-  long  largest = i;
-  long  left = 2 * i + 1;
-  long  right = 2 * i + 2;
-  int   rc;
-
-  if (left < n) {
-    if (list->type == LIST_NAMEVALUE) {
-      rc = nameValueCompare (list->data [left], list->data [largest], compare);
-    } else {
-      rc = compare (list->data [left], list->data [largest]);
-    }
-    if (rc > 0) {
-      largest = left;
-    }
-  }
-
-  if (right < n) {
-    if (list->type == LIST_NAMEVALUE) {
-      rc = nameValueCompare (list->data [right], list->data [largest], compare);
-    } else {
-      rc = compare (list->data [right], list->data [largest]);
-    }
-    if (rc > 0) {
-      largest = right;
-    }
-  }
-
-  if (largest != i) {
-    listSwap (list, i, largest);
-    heapify (list, n, largest, compare);
-  }
-}
-
-// Main function to do heap sort
-static void
-heapSort (list_t *list, listCompare_t compare)
-{
-  // Build max heap
-  for (long i = (long) list->count / 2 - 1; i >= 0; i--) {
-    heapify (list, (long) list->count, i, compare);
-  }
-
-  // Heap sort
-  for (long i = (long) list->count - 1; i >= 0; i--) {
-    listSwap (list, 0, i);
-
-    // Heapify root element to get highest element at root again
-    heapify (list, i, 0, compare);
+  if (l < r) {
+    size_t m = l + (r - l) / 2;
+    mergeSort (list, l, m, compare);
+    mergeSort (list, m + 1, r, compare);
+    merge (list, l, m, r, compare);
   }
 }
