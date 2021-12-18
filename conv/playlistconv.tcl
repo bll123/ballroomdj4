777ce712980c0513a763aaca4853e0164cde7666
Brad Lanam <brad.lanam.di@gmail.com>
2021-12-18 15:12:43 -0800

    conversion progs

---
+++ conv/playlistconv.tcl
@@ -0,0 +1,27 @@
+#!/usr/bin/tclsh
+
+if { $argc <= 0 } {
+  puts "usage: playlistconv <directory>"
+  exit 1
+}
+
+set flist [glob -directory [lindex $argv 0] *.playlist]
+
+foreach {fn} $flist {
+  set ifh [open $fn r]
+  set nfn [file rootname $fn].playlist4
+  set ofh [open $nfn w]
+  puts $ofh "# BDJ4 playlist"
+  puts $ofh "# Converted from $fn"
+  puts $ofh "# [clock format [clock seconds] -gmt 1]"
+  while { [gets $ifh line] >= 0 } {
+    regexp {^([^:]*):(.*)$} $line all key value
+    if { $key eq "version" } {
+      set value 10
+    }
+    puts $ofh $key
+    puts $ofh "  $value"
+  }
+  close $ifh
+  close $ofh
+}
