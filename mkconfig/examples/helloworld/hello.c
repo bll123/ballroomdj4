837a06c9abb0611b8b8ce099d60886d75d9b7b9b
Brad Lanam <brad.lanam.di@gmail.com>
2021-12-18 14:10:04 -0800

    mkconfig

---
+++ mkconfig/examples/helloworld/hello.c
@@ -0,0 +1,13 @@
+#include "config.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#ifdef _hdr_string
+# include <string.h>
+#endif
+#include "hello.h"
+
+int main (int argc, char *argv[])
+{
+  printf ("hello world\n");
+}
