d92b2d6e1d83a07fa2c8a416c795a548b38895bc
Brad Lanam <brad.lanam.di@gmail.com>
2021-12-19 15:41:25 -0800

    put the dbread in a separate thread

--- src/bdj4main.c
+++ src/bdj4main.c
@@ -6,14 +6,31 @@
 #if _hdr_libintl
 # include <libintl.h>
 #endif
+#if _hdr_pthread
+# include <pthread.h>
+#endif
 
 #include "musicdb.h"
 #include "tagdef.h"
 #include "bdjstring.h"
+#include "sysvars.h"
+
+void *openDatabase (void *);
+
+void *
+openDatabase (void *id)
+{
+  dbOpen ("data/musicdb.dat");
+  pthread_exit (NULL);
+}
 
 int
 main (int argc, char *argv[])
 {
+#if _lib_pthread_create
+  pthread_t   thread;
+#endif
+
   // darwin: add /opt/local/bin to the path
   // windows: make sure the ./bin directory is in the path (for .dlls)
   // darwin: set DYLD_FALLBACK_LIBRARY_PATH to:
@@ -24,6 +41,19 @@ main (int argc, char *argv[])
   textdomain ("bdj");
 
   tagdefInit ();
-  dbOpen ("data/musicdb.dat");
+#if _lib_pthread_create
+  /* put the database read into a separate thread */
+  pthread_create (&thread, NULL, openDatabase, NULL);
+#else
+  int junk;
+  openDatabase (&junk);
+#endif
+
+  sysvarsInit ();
+
+#if _lib_pthread_join
+  pthread_join (thread, NULL);
+#endif
+printf ("count: %d\n", dbCount ());
   return 0;
 }
