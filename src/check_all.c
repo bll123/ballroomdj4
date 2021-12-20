22bd97a536710a4a579de667efdc7cb1b0424287
Brad Lanam <brad.lanam.comp@gmail.com>
2021-12-20 13:33:08 -0800

    check tmutil

--- src/check_all.c
+++ src/check_all.c
@@ -22,6 +22,8 @@ main (void)
   srunner_add_suite (sr, s);
   s = sock_suite();
   srunner_add_suite (sr, s);
+  s = tmutil_suite();
+  srunner_add_suite (sr, s);
   srunner_run_all (sr, CK_ENV);
   number_failed = srunner_ntests_failed (sr);
   srunner_free (sr);
---
+++ src/check_tmutil.c
@@ -0,0 +1,56 @@
+#include "bdjconfig.h"
+
+#include <stdio.h>
+#include <check.h>
+#include <sys/types.h>
+#include <time.h>
+
+#include "tmutil.h"
+#include "check_bdj.h"
+
+
+START_TEST(msleep_chk)
+{
+  time_t      tm_s;
+  time_t      tm_e;
+
+  tm_s = time (NULL);
+  msleep (2000);
+  tm_e = time (NULL);
+  ck_assert (tm_e - tm_s >= 2);
+}
+END_TEST
+
+START_TEST(milli_start_end)
+{
+  time_t      tm_s;
+  time_t      tm_e;
+  mtime_t     mi;
+
+  mtimestart (&mi);
+  tm_s = time (NULL);
+  msleep (2000);
+  tm_e = time (NULL);
+  size_t m = mtimeend (&mi);
+  long d = (long) ((tm_e - tm_s) * 1000 - (time_t) m);
+  if (d < 0) {
+    d = - d;
+  }
+  ck_assert (d < 20);
+}
+END_TEST
+
+Suite *
+tmutil_suite (void)
+{
+  Suite     *s;
+  TCase     *tc;
+
+  s = suite_create ("Time Utils Suite");
+  tc = tcase_create ("Time Utils");
+  tcase_add_test (tc, msleep_chk);
+  tcase_add_test (tc, milli_start_end);
+  suite_add_tcase (s, tc);
+  return s;
+}
+
