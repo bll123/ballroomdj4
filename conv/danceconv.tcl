51030eadb82cd4dcf4f39b110dea910c0ef38d73
Brad Lanam <brad.lanam.comp@gmail.com>
2021-12-21 06:10:49 -0800

    more conversion programs

---
+++ conv/danceconv.tcl
@@ -0,0 +1,31 @@
+#!/usr/bin/tclsh
+
+if { $argc <= 0 } {
+  puts "usage: $argv0 <directory>"
+  exit 1
+}
+
+set dir [lindex $argv 0]
+if { ! [file exists $dir] || ! [file isdirectory $dir] } {
+  puts "Invalid directory $dir"
+  exit 1
+}
+
+source [file join $dir dancedefaults.tcl]
+set nfn [file join data dances.txt]
+set fh [open $nfn w]
+puts $fh "# BDJ4 dances"
+puts $fh "# [clock format [clock seconds] -gmt 1]"
+puts $fh version
+puts $fh "..2"
+puts $fh count
+puts $fh "..[expr {[llength $Dance]/2}]"
+foreach {key data} $Dance {
+  puts $fh "DANCE\n..$key"
+  foreach {k v} $data {
+    puts $fh $k
+    puts $fh "..$v"
+  }
+}
+close $fh
+
---
+++ conv/sortoptconv.tcl
@@ -0,0 +1,24 @@
+#!/usr/bin/tclsh
+
+if { $argc <= 0 } {
+  puts "usage: $argv0 <directory>"
+  exit 1
+}
+
+set dir [lindex $argv 0]
+if { ! [file exists $dir] || ! [file isdirectory $dir] } {
+  puts "Invalid directory $dir"
+  exit 1
+}
+
+source [file join $dir sortopt.tcl]
+set nfn [file join data sortopt.txt]
+set fh [open $nfn w]
+puts $fh "# BDJ4 sort options"
+puts $fh "# [clock format [clock seconds] -gmt 1]"
+puts $fh "# version 2"
+foreach {item} $sortOptionList {
+  puts $fh $item
+}
+close $fh
+
