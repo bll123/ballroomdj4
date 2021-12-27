76dc5683cba5735356acecf0206804ad61122ccd
Brad Lanam <brad.lanam.comp@gmail.com>
2021-12-27 13:04:00 -0800

    redo conv types

--- conv/sortoptconv.tcl
+++ conv/sortoptconv.tcl
@@ -18,7 +18,7 @@ set fh [open $nfn w]
 puts $fh "# BDJ4 sort options"
 puts $fh "# [clock format [clock seconds] -gmt 1]"
 puts $fh "datafiletype"
-puts $fh "..simple"
+puts $fh "..list"
 puts $fh "version"
 puts $fh "..2"
 foreach {item} $sortOptionList {
