22448bb09d50337a44380138e8c191912a032658
Brad Lanam <brad.lanam.comp@gmail.com>
2021-12-31 10:31:43 -0800

    comms draft

--- src/sockh.h
+++ src/sockh.h
@@ -5,13 +5,14 @@
 
 #include "sock.h"
 
-typedef void (*sockProcessMsg_t)(long, long, char *);
+typedef int (*sockProcessMsg_t)(long, long, char *);
 typedef void (*sockOtherProcessing_t)(void);
 
 #define SOCK_MAINLOOP_TIMEOUT   5
 
 void    sockhMainLoop (uint16_t listenPort, sockProcessMsg_t msgProc,
             sockOtherProcessing_t);
+int     sockhSendMessage (Sock_t sock, long route, long msg, char *args);
 void    sockhRequestClose (sockinfo_t *sockinfo);
 void    sockhCloseClients (sockinfo_t *sockinfo);
 
