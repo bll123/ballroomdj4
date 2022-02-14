ba938658ebe670c2db24e97637fba873fc7cebb8
Brad Lanam <brad.lanam.di@gmail.com>
2022-02-14 06:34:24 -0800

    beginnings of .po files

---
+++ src/po/lang-lookup.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+
+export olddata
+
+function readolddata {
+  pof=$1
+
+  start=F
+  while read line; do
+    case $line in
+      "")
+        start=T
+        ;;
+    esac
+
+    if [[ $start != T ]]; then
+      continue
+    fi
+
+    case $line in
+      msgid*)
+        omid=$line
+        ;;
+      msgstr*)
+        olddata[$omid]=$line
+        ;;
+      *)
+        ;;
+    esac
+
+  done < $pof
+}
+
+for pofile in *.po; do
+  if [[ $pofile == "en.po" ]]; then
+    continue
+  fi
+
+  olpo=""
+  if [[ -f old/$pofile ]]; then
+    oldpo=old/$pofile
+  else
+    pfx=$(echo $pofile | sed 's/\(..\).*/\1/')
+    oldpo=$(echo old/$pfx*)
+  fi
+
+  unset olddata
+  declare -A olddata
+  readolddata $oldpo
+
+  echo "processing $pofile : $oldpo"
+
+  start=F
+  while read line; do
+    case $line in
+      "")
+        start=T
+        ;;
+    esac
+
+    if [[ $start == F ]]; then
+      echo $line
+      continue
+    fi
+
+    case $line in
+      msgid*)
+        nmid=$line
+        echo $line
+        ;;
+      'msgstr ""')
+        if [[ ${olddata[$nmid]} != "" ]]; then
+          line=${olddata[$nmid]}
+        fi
+        echo $line
+        ;;
+      *)
+        echo $line
+        ;;
+    esac
+
+  done < $pofile > $pofile.n
+  mv -f $pofile.n $pofile
+
+done
