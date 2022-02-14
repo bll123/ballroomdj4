b55d4c2da8ce85eb324dd3548f627bd6517a544a
Brad Lanam <brad.lanam.di@gmail.com>
2022-02-14 06:41:58 -0800

    po updates

--- src/po/install.sh
+++ src/po/install.sh
@@ -2,8 +2,8 @@
 
 rm -rf ../../locale
 
-for i in *.po; do 
+for i in *.po; do
   j=$(echo $i | sed 's,.po$,,')
   mkdir -p ../../locale/$j/LC_MESSAGES
-  msgfmt -o ../../locale/$j/LC_MESSAGES/bdj.mo $i
+  msgfmt -c -o ../../locale/$j/LC_MESSAGES/bdj4.mo $i
 done
