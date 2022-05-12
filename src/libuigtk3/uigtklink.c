4cf83bc7079d32fd97c5f7dd7294fe54fdc3e92a
Brad Lanam <brad.lanam.comp@gmail.com>
2022-05-12 08:22:03 -0700

    ui: uiwidget changes (link)

--- src/libuigtk3/uigtklink.c
+++ src/libuigtk3/uigtklink.c
@@ -22,8 +22,12 @@ uiCreateLink (UIWidget *uiwidget, char *label, char *uri)
   GtkWidget *lwidget;
 
   widget = gtk_link_button_new ("");
-  gtk_link_button_set_uri (GTK_LINK_BUTTON (widget), uri);
-  gtk_button_set_label (GTK_BUTTON (widget), label);
+  if (uri != NULL) {
+    gtk_link_button_set_uri (GTK_LINK_BUTTON (widget), uri);
+  }
+  if (label != NULL) {
+    gtk_button_set_label (GTK_BUTTON (widget), label);
+  }
   lwidget = gtk_bin_get_child (GTK_BIN (widget));
   gtk_label_set_xalign (GTK_LABEL (lwidget), 0.0);
   uiwidget->widget = widget;
@@ -32,6 +36,10 @@ uiCreateLink (UIWidget *uiwidget, char *label, char *uri)
 void
 uiLinkSet (UIWidget *uilink, char *label, char *uri)
 {
-  gtk_link_button_set_uri (GTK_LINK_BUTTON (uilink->widget), uri);
-  gtk_button_set_label (GTK_BUTTON (uilink->widget), label);
+  if (uri != NULL) {
+    gtk_link_button_set_uri (GTK_LINK_BUTTON (uilink->widget), uri);
+  }
+  if (label != NULL) {
+    gtk_button_set_label (GTK_BUTTON (uilink->widget), label);
+  }
 }
