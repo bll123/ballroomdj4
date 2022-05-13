78fc780f5b564e818d9e09bdf27be46946d2b3f2
Brad Lanam <brad.lanam.comp@gmail.com>
2022-05-13 11:51:17 -0700

    sequence/duallist draft a

--- src/include/sequence.h
+++ src/include/sequence.h
@@ -8,9 +8,9 @@ typedef struct {
   nlist_t          *sequence;
 } sequence_t;
 
-sequence_t    *sequenceAlloc (const char *);
-void          sequenceFree (sequence_t *);
-nlist_t       *sequenceGetDanceList (sequence_t *);
+sequence_t    *sequenceAlloc (const char *fname);
+void          sequenceFree (sequence_t *sequence);
+nlist_t       *sequenceGetDanceList (sequence_t *sequence);
 void          sequenceStartIterator (sequence_t *sequence, nlistidx_t *idx);
 nlistidx_t    sequenceIterate (sequence_t *sequence, nlistidx_t *idx);
 
