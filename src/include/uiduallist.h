2cb01119c025ee4339fb865279d4fb4c2f7bc6a8
Brad Lanam <brad.lanam.di@gmail.com>
2022-05-13 09:47:34 -0700

    prep for duallist intfc b

--- src/include/uiduallist.h
+++ src/include/uiduallist.h
@@ -3,15 +3,26 @@
 
 #include "uiutils.h"
 
+enum {
+  DUALLIST_TREE_LEFT,
+  DUALLIST_TREE_RIGHT,
+  DUALLIST_TREE_MAX,
+};
+
 typedef struct {
-  GtkWidget         *ltree;
-  GtkTreeSelection  *lsel;
-  GtkWidget         *rtree;
-  GtkTreeSelection  *rsel;
+  GtkWidget         *tree;
+  GtkTreeSelection  *sel;
+} uiduallisttree_t;
+
+typedef struct {
+  uiduallisttree_t  trees [DUALLIST_TREE_MAX];
+  UICallback        moveprevcb;
+  UICallback        movenextcb;
+  bool              changed : 1;
 } uiduallist_t;
 
 uiduallist_t * uiCreateDualList (UIWidget *vbox,
-    UICallback *uiselectcb, UICallback *uiremovecb,
-    UICallback *uimoveupcb, UICallback *uimovedowncb);
+    UICallback *uiselectcb, UICallback *uiremovecb);
+void uiduallistSet (uiduallist_t *, slist_t *slist, int which);
 
 #endif /* INC_UIDUALLIST_H */
