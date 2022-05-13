1bff087427fdc03776d8fbe160acb77f8054a320
Brad Lanam <brad.lanam.comp@gmail.com>
2022-05-13 09:47:34 -0700

    prep for duallist intfc b

--- src/libbdj4ui/uiduallist.c
+++ src/libbdj4ui/uiduallist.c
@@ -13,14 +13,30 @@
 #include <gtk/gtk.h>
 
 #include "bdj4intl.h"
+#include "slist.h"
 #include "ui.h"
 #include "uiduallist.h"
 #include "uiutils.h"
 
+enum {
+  DUALLIST_COL_DISP,
+  DUALLIST_COL_SB_PAD,
+  DUALLIST_COL_DISP_IDX,
+  DUALLIST_COL_MAX,
+};
+
+enum {
+  DUALLIST_MOVE_PREV = -1,
+  DUALLIST_MOVE_NEXT = 1,
+};
+
+static void uiduallistMovePrev (void *tduallist);
+static void uiduallistMoveNext (void *tduallist);
+static void uiduallistMove (uiduallist_t *duallist, int which, int dir);
+
 uiduallist_t *
 uiCreateDualList (UIWidget *vbox,
-    UICallback *uiselectcb, UICallback *uiremovecb,
-    UICallback *uimoveupcb, UICallback *uimovedowncb)
+    UICallback *uiselectcb, UICallback *uiremovecb)
 {
   uiduallist_t  *duallist;
   UIWidget      hbox;
@@ -29,6 +45,13 @@ uiCreateDualList (UIWidget *vbox,
   GtkWidget     *tree;
 
   duallist = malloc (sizeof (uiduallist_t));
+  for (int i = 0; i < DUALLIST_TREE_MAX; ++i) {
+    duallist->trees [i].tree = NULL;
+    duallist->trees [i].sel = NULL;
+  }
+  duallist->changed = false;
+  uiutilsUICallbackInit (&duallist->moveprevcb, uiduallistMovePrev, duallist);
+  uiutilsUICallbackInit (&duallist->movenextcb, uiduallistMoveNext, duallist);
 
   uiutilsUIWidgetInit (&hbox);
   uiutilsUIWidgetInit (&dvbox);
@@ -50,8 +73,9 @@ uiCreateDualList (UIWidget *vbox,
   uiWidgetSetMarginTopW (tree, uiBaseMarginSz * 8);
   uiWidgetExpandVertW (tree);
   uiBoxPackInWindowUW (&uiwidget, tree);
-  duallist->ltree = tree;
-  duallist->lsel = gtk_tree_view_get_selection (GTK_TREE_VIEW (tree));
+  duallist->trees [DUALLIST_TREE_LEFT].tree = tree;
+  duallist->trees [DUALLIST_TREE_LEFT].sel =
+      gtk_tree_view_get_selection (GTK_TREE_VIEW (tree));
 
   uiCreateVertBox (&dvbox);
   uiWidgetSetAllMargins (&dvbox, uiBaseMarginSz * 4);
@@ -81,8 +105,9 @@ uiCreateDualList (UIWidget *vbox,
   uiWidgetSetMarginTopW (tree, uiBaseMarginSz * 8);
   uiWidgetExpandVertW (tree);
   uiBoxPackInWindowUW (&uiwidget, tree);
-  duallist->rtree = tree;
-  duallist->rsel = gtk_tree_view_get_selection (GTK_TREE_VIEW (tree));
+  duallist->trees [DUALLIST_TREE_RIGHT].tree = tree;
+  duallist->trees [DUALLIST_TREE_RIGHT].sel =
+      gtk_tree_view_get_selection (GTK_TREE_VIEW (tree));
 
   uiCreateVertBox (&dvbox);
   uiWidgetSetAllMargins (&dvbox, uiBaseMarginSz * 4);
@@ -91,14 +116,108 @@ uiCreateDualList (UIWidget *vbox,
   uiBoxPackStart (&hbox, &dvbox);
 
   /* CONTEXT: configuration: display settings: button: move the selected field up */
-  uiCreateButton (&uiwidget, uimoveupcb, _("Move Up"),
+  uiCreateButton (&uiwidget, &duallist->moveprevcb, _("Move Up"),
       "button_up", NULL, NULL);
   uiBoxPackStart (&dvbox, &uiwidget);
 
   /* CONTEXT: configuration: display settings: button: move the selected field down */
-  uiCreateButton (&uiwidget, uimovedowncb, _("Move Down"),
+  uiCreateButton (&uiwidget, &duallist->movenextcb, _("Move Down"),
       "button_down", NULL, NULL);
   uiBoxPackStart (&dvbox, &uiwidget);
 
   return duallist;
 }
+
+void
+uiduallistSet (uiduallist_t *duallist, slist_t *slist, int which)
+{
+  GtkWidget     *tree;
+  GtkListStore  *store;
+  GtkTreeIter   iter;
+  char          *keystr;
+  slistidx_t    siteridx;
+
+
+  if (which >= DUALLIST_TREE_MAX) {
+    return;
+  }
+
+  tree = duallist->trees [which].tree;
+  store = gtk_list_store_new (DUALLIST_COL_MAX,
+      G_TYPE_STRING, G_TYPE_STRING, G_TYPE_ULONG);
+  assert (store != NULL);
+
+  slistStartIterator (slist, &siteridx);
+  while ((keystr = slistIterateKey (slist, &siteridx)) != NULL) {
+    int val;
+
+    val = slistGetNum (slist, keystr);
+    gtk_list_store_append (store, &iter);
+    gtk_list_store_set (store, &iter,
+        DUALLIST_COL_DISP, keystr,
+        DUALLIST_COL_SB_PAD, "    ",
+        DUALLIST_COL_DISP_IDX, (gulong) val,
+        -1);
+  }
+
+  gtk_tree_view_set_model (GTK_TREE_VIEW (tree), GTK_TREE_MODEL (store));
+  g_object_unref (store);
+}
+
+static void
+uiduallistMovePrev (void *tduallist)
+{
+  uiduallist_t  *duallist = tduallist;
+  uiduallistMove (duallist, DUALLIST_TREE_LEFT, DUALLIST_MOVE_PREV);
+}
+
+static void
+uiduallistMoveNext (void *tduallist)
+{
+  uiduallist_t  *duallist = tduallist;
+  uiduallistMove (duallist, DUALLIST_TREE_LEFT, DUALLIST_MOVE_NEXT);
+}
+
+void
+uiduallistMove (uiduallist_t *duallist, int which, int dir)
+{
+  GtkWidget         *tree;
+  GtkTreeSelection  *sel;
+  GtkTreeModel      *model;
+  GtkTreeIter       iter;
+  GtkTreeIter       citer;
+  GtkTreePath       *path;
+  char              *pathstr;
+  int               count;
+  gboolean          valid;
+  int               idx;
+
+  tree = duallist->trees [which].tree;
+  sel = duallist->trees [which].sel;
+  count = gtk_tree_selection_count_selected_rows (sel);
+  if (count != 1) {
+    return;
+  }
+  gtk_tree_selection_get_selected (sel, &model, &iter);
+
+  path = gtk_tree_model_get_path (model, &iter);
+  pathstr = gtk_tree_path_to_string (path);
+  sscanf (pathstr, "%d", &idx);
+  free (pathstr);
+  gtk_tree_path_free (path);
+
+  memcpy (&citer, &iter, sizeof (iter));
+  if (dir == DUALLIST_MOVE_PREV) {
+    valid = gtk_tree_model_iter_previous (model, &iter);
+    if (valid) {
+      gtk_list_store_move_before (GTK_LIST_STORE (model), &citer, &iter);
+    }
+  } else {
+    valid = gtk_tree_model_iter_next (model, &iter);
+    if (valid) {
+      gtk_list_store_move_after (GTK_LIST_STORE (model), &citer, &iter);
+    }
+  }
+  duallist->changed = true;
+}
+
