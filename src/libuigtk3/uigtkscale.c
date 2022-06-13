2256db8ff8b598e11e7d22cb093263758ade409a
Brad Lanam <brad.lanam.di@gmail.com>
2022-06-13 11:57:12 -0700

    fix scale inversion; fix scale crash in song editor; fix player scale incs

--- src/libuigtk3/uigtkscale.c
+++ src/libuigtk3/uigtkscale.c
@@ -23,7 +23,8 @@ uiCreateScale (UIWidget *uiwidget, double lower, double upper,
   GtkWidget     *scale;
   GtkAdjustment *adjustment;
 
-  adjustment = gtk_adjustment_new (0.0, lower, upper, stepinc, pageinc, 0.0);
+  /* due to the gtk scale inversion bug, invert the stepinc and the pageinc */
+  adjustment = gtk_adjustment_new (0.0, lower, upper, - stepinc, - pageinc, 0.0);
   scale = gtk_scale_new (GTK_ORIENTATION_HORIZONTAL, adjustment);
   assert (scale != NULL);
   gtk_widget_set_size_request (scale, 200, 5);
@@ -33,6 +34,7 @@ uiCreateScale (UIWidget *uiwidget, double lower, double upper,
   /* to align it properly */
   gtk_scale_set_draw_value (GTK_SCALE (scale), FALSE);
   gtk_scale_set_digits (GTK_SCALE (scale), digits);
+  gtk_range_set_round_digits (GTK_RANGE (scale), digits);
   gtk_scale_set_has_origin (GTK_SCALE (scale), TRUE);
   gtk_range_set_value (GTK_RANGE (scale), initvalue);
   uiSetCss (scale, "scale, trough { min-height: 5px; }");
@@ -100,7 +102,7 @@ static gboolean
 uiScaleChangeValueHandler (GtkRange *range, GtkScrollType *scroll, gdouble value, gpointer udata)
 {
   UICallback  *uicb = udata;
-  bool        rc = UICB_STOP;
+  bool        rc = UICB_CONT;
 
   if (uicb == NULL) {
     return rc;
