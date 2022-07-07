f051d57d16cb755b1194996c76b71a2ac91dc37f
Brad Lanam <brad.lanam.comp@gmail.com>
2022-07-06 20:29:08 -0700

    fixes for windows checksums; update msgs

--- pkg/mkchecksum.sh
+++ pkg/mkchecksum.sh
@@ -8,27 +8,51 @@ systype=$(uname -s)
 shaprog=sha512sum
 win=F
 case ${systype} in
+  Linux)
+    nproc=$(getconf _NPROCESSORS_ONLN)
+    ;;
   Darwin)
+    nproc=$(getconf _NPROCESSORS_ONLN)
     shaprog="shasum -a 512"
     ;;
   MINGW64*|MINGW32*)
+    nproc=$NUMBER_OF_PROCESSORS
     win=T
     ;;
 esac
 
+inpfx=ckin
+outpfx=ckout.
+
+rm -f tmp/${inpfx}??
+rm -f tmp/${outpfx}*
+
+# building the checksum file on windows is slow.
+# this really isn't necessary on linux/macos.
+split -n l/$nproc ${manifest} tmp/${inpfx}
+
 (
-  for fn in $(cat ${manifest}); do
+cd $(dirname $(dirname ${manifest}))
+count=0
+for cfn in ../${inpfx}??; do
+  count=$((count+1))
+  for fn in $(cat ${cfn}); do
     if [[ -f $fn ]]; then
-      ${shaprog} -b $fn
+      ${shaprog} -b $fn >> ../${outpfx}${count} &
     fi
   done
-) > ${checksumfn}
+done
+)
+
+cat tmp/${outpfx}* > ${checksumfn}
+rm -f tmp/${inpfx}??
+rm -f tmp/${outpfx}*
 
 if [[ $win == T ]]; then
   # for windows, the '*' binary indicator before the filename needs
   # to be removed so that the checksum verification batch file is
   # much simpler.
-  ed ${checksumfn} << _HERE_
+  ed ${checksumfn} << _HERE_ > /dev/null
 1,$ s, \*, ,
 w
 q
