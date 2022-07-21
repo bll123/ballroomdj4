0d1a8ec148f3cce0299c315e890632644fd99b13
Brad Lanam <brad.lanam.di@gmail.com>
2022-07-21 10:51:01 -0700

    delete profile

--- src/include/filemanip.h
+++ src/include/filemanip.h
@@ -11,7 +11,6 @@ enum {
 int     filemanipCopy (const char *from, const char *to);
 int     filemanipLinkCopy (const char *from, const char *to);
 int     filemanipMove (const char *from, const char *to);
-void    filemanipDeleteDir (const char *dir);
 slist_t * filemanipBasicDirList (const char *dir, const char *extension);
 slist_t * filemanipRecursiveDirList (const char *dir, int flags);
 void    filemanipBackup (const char *fname, int count);
