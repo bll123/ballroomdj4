a97d642efd9627c6c1059c921ad16d87d3f32c79
Brad Lanam <brad.lanam.comp@gmail.com>
2022-09-17 11:04:25 -0700

    lksjdfldjsfljadofjaofjasdfjasdjf!!!

--- src/i-pl.sh
+++
@@ -1,2 +0,0 @@
-./itunes /home/bll/vbox_shared/iTunes\ Music\ Library.xml \
-  '/plist/dict/array/dict/key|/plist/dict/array/dict/string|/plist/dict/array/dict/array/dict/key|/plist/dict/array/dict/array/dict/integer'
--- src/zz.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for f in dances.txt \
-    ; do
-  echo "=== $f"
-  diff -u ../data/${f}.bak.1 ../data/${f}
-done
